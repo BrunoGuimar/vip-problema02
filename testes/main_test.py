17146a279c1e964ebc79cc425a22cd7a8ec02da5
Bruno Guimarães <brunoguima13@gmail.com>
2023-08-30 18:02:32 -0300

    Adicionando diretório testes

--- testes/main_test.py
+++ testes/main_test.py
@@ -1,4 +1,4 @@
-import main
+from src.main import _calc_meses, calc_valor_futuro, calc_valor_presente
 from faker import Faker
 
 faker = Faker()
@@ -7,7 +7,7 @@ faker = Faker()
 def teste_calc_meses():
     # Variáveis de teste na função privada
     esperado = 12
-    resultado = main._calc_meses("2024-08-30")
+    resultado = _calc_meses("2024-08-30")
 
     # Deve retornar True
     assert resultado == esperado
@@ -17,24 +17,24 @@ def teste_calc_valor_presente():
     # Geração de valores aleatórios
     valor_futuro_aleatorio = faker.random_int(1000, 10000)
     data_futura_aleatorio = faker.future_date("+1y")
-    qtd_meses = main._calc_meses(data_futura_aleatorio)
+    qtd_meses = _calc_meses(data_futura_aleatorio)
 
     # Variáveis de teste
-    resultado = main.calc_valor_presente(valor_futuro_aleatorio, 0.5, data_futura_aleatorio)
+    resultado = calc_valor_presente(valor_futuro_aleatorio, 0.5, data_futura_aleatorio)
     esperado = valor_futuro_aleatorio / (1 + 0.5) ** qtd_meses
 
     # Deve retornar True
-    assert resultado == round(esperado)
+    assert round(resultado) == round(esperado)
 
 
 def teste_calc_valor_futuro():
     # Geração de valores aleatórios
     valor_presente_aleatorio = faker.random_int(1000, 10000)
     data_futura_aleatorio = faker.future_date("+1y")
-    qtd_meses = main._calc_meses(data_futura_aleatorio)
+    qtd_meses = _calc_meses(data_futura_aleatorio)
 
     # Variáveis de teste
-    resultado = main.calc_valor_futuro(valor_presente_aleatorio, 0.5, data_futura_aleatorio)
+    resultado = calc_valor_futuro(valor_presente_aleatorio, 0.5, data_futura_aleatorio)
     esperado = valor_presente_aleatorio * (1 + 0.5) ** qtd_meses
 
     # Deve retornar True
